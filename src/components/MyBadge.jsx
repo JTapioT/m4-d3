7ef7eea4991cee504185372f8857eceebd571edb
JTapioT <88656150+JTapioT@users.noreply.github.com>
2021-09-29 20:31:32 +0200

    Small update
    Cards now get border when clicked. There still might be something with the logic wrong as card needs to be double-clicked to make it actually show border.

--- src/components/MyBadge.jsx
+++ src/components/MyBadge.jsx
@@ -2,11 +2,7 @@ import Badge from "react-bootstrap/Badge";
 
 const MyBadge = (props) => {
   return (
-    <div>
-      <h1>
-        <Badge  style={{backgroundColor:props.color}}>This is a badge</Badge>
-      </h1>
-    </div>
+    <Badge  style={{backgroundColor:props.color}}>This is a badge</Badge>
   );
 };
 
--- src/components/SingleBook .jsx
+++ src/components/SingleBook .jsx
@@ -5,13 +5,21 @@ class SingleBook extends React.Component {
   
     state = {
         selected : false,
+        cardBorder : {}
     }
-  
+    
+    // Managed to get the cards show border when clicked and also make border disappear when clicking on the card again.
+    // I still think that logic is somehow failing, I need to double-click for card to change the border.
   
     render() {
     return (
-      <div className="col-3" style={{ padding: "0.3rem" }} onClick={() => {this.setState({selected: true})}}>
-        <Card>
+      <div className="col-3" style={{ padding: "0.3rem", cursor: "pointer"}} onClick={() => {
+        this.setState({
+          selected: this.state.selected ? false : true,
+          cardBorder: this.state.selected ? {border: "2px solid black"} : {},
+        });
+      }}>
+          <Card style={this.state.cardBorder}>
           <Card.Img
             className="img-fluid"
             src={this.props.book.img}
